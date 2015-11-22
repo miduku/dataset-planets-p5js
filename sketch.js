ec71a6f9ee35079f2c48535d7aa70218e6359164
Dustin K <miduku11@gmail.com>
2015-11-22 19:53:20 +0100

    feed me planet data

--- sketch.js
+++ sketch.js
@@ -9,10 +9,22 @@ var h = 360,
     b = 90,
     a = 100;
 
+var bodies;
+
+/*
+* preload
+*/
+function preload() {
+  bodies = loadJSON('assets/json/planets.json');
+}
+
 /*
 * only executed once
 */
 function setup(){
+  var planets, 
+        name = [],    // planet names
+        moons = [];   // moon names
   // change color mode to hsl
   colorMode(HSB, 360, 100, 100, 100);
   var bgrColor = color(h, s, b, a);
@@ -22,6 +34,16 @@ function setup(){
 
   // rectangle/background
   background(bgrColor);
+
+  // main key
+  planets = bodies.planets;
+
+  for (var key in planets) {
+    name.push(planets[key].name);
+    moons.push(planets[key].moons);
+  }
+
+  console.log(planets);
 }
 
 
